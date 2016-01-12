d4814aee91f6c8a9a149c1d5bf6124567ce2807d
wildman2bad <danutzcodrescu@gmail.com>
2016-01-12 21:55:47 +0000

    seeds Contact and Contact index fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.destroy_all
+
+
+title=["Mr", "Ms", "Prof", "Dr"]
+
+50.times do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    prefix = title.sample
+    organization = Faker::Company.name
+    position = Faker::Name.title
+    email = Faker::Internet.email
+    Contact.create!(First_Name: first_name, Last_Name: last_name, Title: prefix, Organization: organization, Position: position, Email: email)
+end
